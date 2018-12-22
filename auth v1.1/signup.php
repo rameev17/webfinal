19590b43fb74e26a121efdc859b05a4f45fca1f2
rameev17 <43203919+rameev17@users.noreply.github.com>
2018-12-23 01:43:29 +0600

    Add files via upload

---
+++ auth v1.1/signup.php
@@ -0,0 +1,43 @@
+<?php
+$username = $_POST['username'];
+$pass = $_POST['pass'];
+$phone = $_POST['phone'];
+
+if (!empty($username)||!empty($pass)||!empty($phone)) {
+	$host="localhost";
+	$dbUsername="root";
+	$dbPassword="";
+	$dbname="123";
+	
+	$conn = new mysqli($host,$dbUsername,$dbPassword,$dbname);
+	$INSERT ="INSERT INTO register (username,pass,phone) VALUES(?,?,?)";
+	$SELECT="SELECT username FROM register WHERE username=? LIMIT 1";
+	if (mysqli_connect_errno()) {
+    printf("Не удалось подключиться: %s\n", mysqli_connect_error());
+    exit();
+}
+
+	$stmt=$conn->prepare($SELECT);
+	$stmt->bind_param("s",$username);
+	$stmt->execute();
+	$stmt->bind_result($username);
+	$stmt->store_result();
+	$rnum=$stmt->num_rows;
+
+	if($rnum==0){
+		$stmt->close();
+
+		$stmt=$conn->prepare($INSERT);
+		$stmt->bind_param("ssi",$username, $pass, $phone);
+		$stmt->execute();
+		echo "sucessfull";
+	}
+	else{
+		echo "email is used";
+	}
+}
+else{
+	echo "All field are required";
+	die();
+}
+?>
